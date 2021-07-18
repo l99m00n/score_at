b0472bb1322ba4ac53c19e095ac900c0a859295a
lmnoon <tak.hat020712@gmail.com>
2021-07-18 20:36:48 +0900

    Laravel Breezeによる認証の導入

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('twitter_id');
+            $table->string('twitter_id')->nullable()->default(null);
             $table->rememberToken();
             $table->timestamps();
         });
